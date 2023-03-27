a751aaacabd790bdd0b1d2df67dc1947e04fbc39
SuyeonParka <sysysy3331@gmail.com>
2023-03-27 18:08:07 +0900

    0327 edu up

--- PHP/Example/05_1_ex1_array.php
+++ PHP/Example/05_1_ex1_array.php
@@ -43,15 +43,73 @@
     // echo $arr_temp[1][3];
 
     //현업에서는 우리가 필요한 변수를 먼저 담고 값을 가져옴
-    $arr_temp_3 = $arr_temp[2][0];
+    // $arr_temp_3 = $arr_temp[2][0];
     // var_dump($arr_temp_3);
 
-    $arr_temp_3_c = array(
-                    array(9, 10, 11)
-                    );
+    // $arr_temp_3_c = array(
+    //                 array(9, 10, 11)
+    //                 );
 
     //배열의 원소 삭제 : 함수unset() 키값 자체가 사라짐
-    $arr_week = array("Sun", "Mon", "delete", "Tue", "Wed");
-    unset($arr_week[2]);
-    print_r($arr_week);
+    // $arr_week = array("Sun", "Mon", "delete", "Tue", "Wed");
+    // unset($arr_week[2]);
+    // print_r($arr_week);
+
+    //array_diff() 중복되지 않는 원소를 반환 
+    //array 커지면 커질수록 속도 주의
+    // $arr_diff_1 = array("a", "b", "c");
+    // $arr_diff_2 = array("a", "b", "d");
+    // $arr_diff = array_diff($arr_diff_1, $arr_diff_2);
+    // print_r($arr_diff);
+
+    //배열의 정렬 : asort(), arsort(), ksort(), krsort();
+    //asort(); 배열 오름차순 정렬
+    // $arr_asort = array("b", "a", "d", "c");
+    // asort($arr_asort);
+    // print_r($arr_asort);
+
+    //arsort(); 배열 내림차순 정렬
+    // $arr_arsort = array("b", "a", "d", "c");
+    // arsort($arr_arsort);
+    // print_r($arr_arsort);
+
+    //ksort(); 오름차순
+    // $arr_ksort = array("key1" => "val1"
+    //                 ,"key3" => "val2"
+    //                 ,"key4" => "val4"
+    //                 ,"key2" => "val3"
+    //             ); 
+    // ksort($arr_ksort);
+    // print_r($arr_ksort);
+
+    //krsort(); 내림차순
+    // $arr_krsort = array("key1" => "val1"
+    //                 ,"key3" => "val2"
+    //                 ,"key4" => "val4"
+    //                 ,"key2" => "val3"
+    //             ); 
+    // krsort($arr_krsort);
+    // print_r($arr_krsort);
+
+    // array의 사이즈를 반환하는 함수 count
+    // echo count($arr_krsort);
+
+    //foreach($array as $key => $val ){};
+    // foreach($array as $val ){};
+    $arr1 = array( "key1" => "val1"
+                    , "key3" => "val2"
+                    , "key4" => "val4"
+                    , "key2" => "val3"
+                ); 
+    // foreach($arr1 as $key => $val)
+    // {
+    //     echo $key." : ".$val."\n";
+    // }
+
+    foreach($arr1 as $val)
+    {
+        echo $val."\n";
+    }
+
+
 ?>
--- PHP/TNG/05_1_tng1_array.php
+++ PHP/TNG/05_1_tng1_array.php
@@ -22,14 +22,41 @@
 // echo $arr_ass["된장찌개"];
 
 //키:김치 원소 삭제
-$arr_ass_del = array (
-                "된장찌개" => "파"
-                , "볶음밥" => "양파"
-                , "김치" => "마늘"
-                , "비빔밥" => "참기름");
-unset($arr_ass_del["김치"]);
-// print_r($arr_ass_del);
-var_dump($arr_ass_del);
+// $arr_ass_del = array (
+//                 "된장찌개" => "파"
+//                 , "볶음밥" => "양파"
+//                 , "김치" => "마늘"
+//                 , "비빔밥" => "참기름"
+//                     );
+// unset($arr_ass_del["김치"]);
+// // print_r($arr_ass_del);
+// var_dump($arr_ass_del);
+
+//foreach문을 이용해서 키가 "삭제"인 요소를 제거해 주세요.
+//if문 사용, unset("삭제") x,키가 "삭제" 이외는 "키 : 값"포맷으로 출력
+$arr1 = array (
+                    "된장찌개" => "파"
+                    , "볶음밥" => "양파"
+                    , "삭제" => "값값"
+                    , "김치" => "마늘"
+                    , "비빔밥" => "참기름"
+                    );
+foreach($arr1 as $key => $val)
+{
+    if ($key !== "삭제")
+    {
+        echo $key.":".$val."\n";
+    }
+    else 
+    {
+        unset($arr1[$key]);
+    }
+}
+
+
+
+
+
 
 
 
