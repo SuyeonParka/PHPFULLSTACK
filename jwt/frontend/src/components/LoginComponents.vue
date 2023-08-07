19060ae48a6cda58e16b94c758fd68ecc9b90b89
SuyeonParka <sysysy3331@gmail.com>
2023-08-07 18:07:06 +0900

    0807

---
+++ jwt/frontend/src/components/LoginComponents.vue
@@ -0,0 +1,13 @@
+<template lang="">
+    <div>
+        <h1>Logincomponents</h1>
+    </div>
+</template>
+<script>
+export default {
+    name: "LoginComponents",
+}
+</script>
+<style lang="">
+    
+</style>
---
+++ jwt/frontend/src/components/MainComponents.vue
@@ -0,0 +1,13 @@
+<template lang="">
+    <div>
+        <h1>Maincomponents</h1>
+    </div>
+</template>
+<script>
+export default {
+    name: "MainComponents",
+}
+</script>
+<style lang="">
+    
+</style>
